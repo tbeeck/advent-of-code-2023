6070451311d7257c0d1efe4a110c50dbcac88c2c
Tim Beck <becktimd@gmail.com>
2024-09-15 21:57:24 -0700

    close

--- src/day02.clj
+++ src/day02.clj
@@ -9,18 +9,45 @@
                   :blue 14})
 
 (def pair-pattern (re-pattern #"\s(\d+) (blue|red|green),?"))
-(def round-pattern (re-pattern (str "(" pair-pattern "){1,3}(;|\n)")))
+(def round-pattern (re-pattern (str "(" pair-pattern "){1,3}[;\n]?")))
+
+; round is the map. Is this an OK grouping?
+(defn round-valid
+  [round]
+  (every?
+   (fn [[k v]]
+     (<= v (get cube-counts k)))
+   round))
+
+(defn group-to-map
+  [group]
+  (let [matches (re-seq pair-pattern (first group))]
+    (into {}
+          (map
+           (fn [item]
+             (let [color (first item)
+                   count (Integer/parseInt (second item))]
+               [(keyword color) count]))
+           (map
+            (fn [match] (reverse (take-last 2 match)))
+            matches)))))
+
+; return list of maps of cubes found in each round of a game
+(defn game-rounds
+  [line]
+  (let [groups (re-seq round-pattern line)]
+    (map group-to-map groups)))
+
+; return the id of the game if it's invalid or 0 if it's valid
+(defn game-valid
+  [line]
+  (let [id (re-find #"Game (\d+)" line)
+        rounds (game-rounds line)]
+    (if (every? round-valid rounds)
+      0
+      (Integer/parseInt id))))
 
 #_{:clj-kondo/ignore [:unused-binding]}
-;; (defn -main [& args]
-;;   (let [lines (read-lines)]
-;;     (doseq [line lines]
-;;       (println line (pair-list line)))))
 (defn -main [& args]
   (let [lines (read-lines)]
-    (doseq [l lines
-            :let [groups (re-seq round-pattern l)]]
-      (println)
-      (doseq [group groups
-              :let [pairs (re-seq pair-pattern (first group))]]
-          (println (first group) (map first pairs))))))
+    (println (reduce + (map game-valid lines)))))
