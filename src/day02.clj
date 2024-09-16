650933a84927cfcc20fb5ebb4649b9df8f22c1cd
Tim Beck <becktimd@gmail.com>
2024-09-15 19:38:54 -0700

    regex debug

--- src/day02.clj
+++ src/day02.clj
@@ -4,29 +4,23 @@
   []
   (doall (line-seq (java.io.BufferedReader. *in*))))
 
-(def cube-counts {"red" 12
-                  "green" 13
-                  "blue" 14})
+(def cube-counts {:red 12
+                  :green 13
+                  :blue 14})
 
 (def pair-pattern (re-pattern #"\s(\d+) (blue|red|green),?"))
-(def group-pattern (re-pattern (str "(" pair-pattern "){1,3}(;|\n)")))
-(println group-pattern)
-
-(defn line-to-groups
-  [line]
-    (println (re-seq group-pattern line))
-    (re-seq pair-pattern
-            (first (first (re-seq group-pattern line)))))
+(def round-pattern (re-pattern (str "(" pair-pattern "){1,3}(;|\n)")))
 
 #_{:clj-kondo/ignore [:unused-binding]}
-(defn -main [& args]
-  (let [lines (read-lines)]
-    (doseq [line lines]
-      (println line (line-to-groups line)))))
 ;; (defn -main [& args]
 ;;   (let [lines (read-lines)]
-;;     (doseq [l lines
-;;             :let [groups (re-seq group-pattern l)]]
-;;       (doseq [group groups
-;;               :let [pairs (re-seq pair-pattern (first group))]]
-;;           (println (first group) (map first pairs))))))
+;;     (doseq [line lines]
+;;       (println line (pair-list line)))))
+(defn -main [& args]
+  (let [lines (read-lines)]
+    (doseq [l lines
+            :let [groups (re-seq round-pattern l)]]
+      (println)
+      (doseq [group groups
+              :let [pairs (re-seq pair-pattern (first group))]]
+          (println (first group) (map first pairs))))))
